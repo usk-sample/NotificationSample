a144bd0f768204c3544e0dc3d842a3bf59440971
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-07-16 15:42:15 +0900

    feat: add notification

--- NotificationSample/ContentView.swift
+++ NotificationSample/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Provisional Authorization")
+                .padding()
+            Button.init("Schedule Notification") {
+                NotificationManager.shared.scheduleNotification()
+            }
+
+        }.onAppear {
+            NotificationManager.shared.requestAuthorization()
+        }
+            
     }
 }
 
---
+++ NotificationSample/NotificationManager.swift
@@ -0,0 +1,52 @@
+//
+//  NotificationManager.swift
+//  NotificationSample
+//
+//  Created by Yusuke Hasegawa on 2021/07/16.
+//
+
+import Foundation
+import UserNotifications
+
+class NotificationManager {
+    
+    static let shared = NotificationManager()
+    
+    private var center: UNUserNotificationCenter {
+        return .current()
+    }
+    
+}
+
+extension NotificationManager {
+    
+    func requestAuthorization() {
+        center.requestAuthorization(options: [.alert, .sound, .badge, .provisional]) { granted, error in
+            if let error = error {
+                debugPrint(error)
+            } else {
+                debugPrint("granted : \(granted)")
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Provisional Authorization"
+        content.subtitle = "subtitle"
+        content.body = "body"
+        
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 10, repeats: false)
+        
+        let request = UNNotificationRequest.init(identifier: "id", content: content, trigger: trigger)
+        
+        center.add(request) { error in
+            if let error = error {
+                debugPrint(error)
+            }
+        }
+        
+    }
+    
+}
