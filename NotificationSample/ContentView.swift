f7db18fa44eb8351a65723cf6cf6a5fa5165bfed
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-07-16 15:25:56 +0900

    Initial Commit

---
+++ NotificationSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotificationSample
+//
+//  Created by Yusuke Hasegawa on 2021/07/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotificationSample/NotificationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationSampleApp.swift
+//  NotificationSample
+//
+//  Created by Yusuke Hasegawa on 2021/07/16.
+//
+
+import SwiftUI
+
+@main
+struct NotificationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
